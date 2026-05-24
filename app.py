20c479243363bd9836889b44e7fa07938aeb4844
burcudsr <68791733+burcudsr@users.noreply.github.com>
2026-05-24 23:21:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from PIL import Image
 
 @st.cache_resource
 def load_my_model():
-    # 1. MİMARİYİ BİRLEŞTİR (3 parça .keras)
+    # 1. MİMARİYİ BİRLEŞTİR VE YÜKLE
     combined_keras = 'full_model.keras'
     with open(combined_keras, 'wb') as outfile:
         for i in range(3): 
@@ -16,8 +16,8 @@ def load_my_model():
                 with open(part_name, 'rb') as infile:
                     outfile.write(infile.read())
     
-    # 2. AĞIRLIKLARI BİRLEŞTİR (9 parça .h5)
-    combined_weights = 'full_weights.h5'
+    # 2. AĞIRLIKLARI BİRLEŞTİR (Senin 'full_weights.h5' burada!)
+    combined_weights = 'full.weights.h5'
     with open(combined_weights, 'wb') as outfile:
         for i in range(9):
             part_name = f'catdog_part{i}.h5'
@@ -25,18 +25,17 @@ def load_my_model():
                 with open(part_name, 'rb') as infile:
                     outfile.write(infile.read())
     
-    # 3. HİBRİT YÜKLEME: Önce mimariyi, sonra ağırlıkları
-    # compile=False kullanarak modelin eğitim ayarlarını yüklemeden sadece yapıyı alıyoruz
+    # 3. YÜKLEME
+    # Önce iskeleti kur
     model = tf.keras.models.load_model(combined_keras, compile=False)
-    
-    # Ağırlıkları mimarinin üzerine güvenle yüklüyoruz
+    # Sonra ağırlıkları üzerine giydir
     model.load_weights(combined_weights)
     
     return model
 
 model = load_my_model()
 
-# ARAYÜZ (Tahmin kodların aynı kalabilir)
+# ARAYÜZ (Tahmin kodların aynı)
 st.title("🐱 Cat vs Dog Classifier")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
