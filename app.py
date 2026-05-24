8610392a3799e9f8bfbea0a7604f449a105dbfc3
burcudsr <68791733+burcudsr@users.noreply.github.com>
2026-05-24 23:17:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,50 +5,45 @@ import numpy as np
 import cv2
 from PIL import Image
 
-# 1. YÜKLEME: Mimariyi .keras, ağırlıkları .h5 dosyalarından yükle
 @st.cache_resource
 def load_my_model():
-    # --- A. Mimariyi Birleştir ve Yükle ---
-    keras_filename = 'full_model_structure.keras'
-    # .keras parçalarının sayısını buraya yaz (örneğin 3 ise range(3))
-    with open(keras_filename, 'wb') as outfile:
+    # 1. MİMARİYİ BİRLEŞTİR (3 parça .keras)
+    combined_keras = 'full_model.keras'
+    with open(combined_keras, 'wb') as outfile:
         for i in range(3): 
-            part_name = f'model_part{i}.keras'
+            part_name = f'catdog_part{i}.keras'
             if os.path.exists(part_name):
                 with open(part_name, 'rb') as infile:
                     outfile.write(infile.read())
     
-    # --- B. Ağırlıkları Birleştir ---
-    weights_filename = 'final_weights.h5'
-    with open(weights_filename, 'wb') as outfile:
+    # 2. AĞIRLIKLARI BİRLEŞTİR (9 parça .h5)
+    combined_weights = 'full_weights.h5'
+    with open(combined_weights, 'wb') as outfile:
         for i in range(9):
             part_name = f'catdog_part{i}.h5'
             if os.path.exists(part_name):
                 with open(part_name, 'rb') as infile:
                     outfile.write(infile.read())
     
-    # --- C. Hibrit Yükleme ---
-    # Mimariyi dosyadan oku (compile=False yaparak hatayı engelliyoruz)
-    model = tf.keras.models.load_model(keras_filename, compile=False)
-    # Ağırlıkları mimarinin üzerine enjekte et
-    model.load_weights(weights_filename)
+    # 3. HİBRİT YÜKLEME: Önce mimariyi, sonra ağırlıkları
+    # compile=False kullanarak modelin eğitim ayarlarını yüklemeden sadece yapıyı alıyoruz
+    model = tf.keras.models.load_model(combined_keras, compile=False)
+    
+    # Ağırlıkları mimarinin üzerine güvenle yüklüyoruz
+    model.load_weights(combined_weights)
     
     return model
 
-# Modeli yükle
 model = load_my_model()
 
-# 2. ARAYÜZ (Aynı kalabilir)
+# ARAYÜZ (Tahmin kodların aynı kalabilir)
 st.title("🐱 Cat vs Dog Classifier")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, use_column_width=True)
-    
-    img_array = np.array(image)
-    img = cv2.resize(img_array, (120, 120))
-    img = img / 255.0
+    img = cv2.resize(np.array(image), (120, 120)) / 255.0
     img = np.expand_dims(img, axis=0)
     
     if st.button("Predict"):
